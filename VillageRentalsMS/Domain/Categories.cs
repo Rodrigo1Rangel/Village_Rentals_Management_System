01a18d2e06efb3569751e7454c51658bf0989737
Rodrigo Rangel <130096450+Rodrigo1Rangel@users.noreply.github.com>
2024-08-11 08:08:55 -0600

    Add skeleton for each class

---
+++ VillageRentalsMS/Domain/Categories.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace VillageRentalsMS.Domain
+{
+    internal class Categories
+    {
+        // =====================================  INSTANCE FIELDS ======================================
+
+
+        // ========================================  PROPERTIES ========================================
+
+
+        // =======================================  CONSTRUCTORS =======================================
+
+
+        // ==========================================  METHODS =========================================
+        
+        /// <summary>
+        /// Blob.
+        /// </summary>
+        /// <param name="value">Blob.</param>
+        /// <returns>Blob.</returns>
+
+
+    }
+}
---
+++ VillageRentalsMS/Domain/EquipmentToSell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillageRentalsMS.Domain
+{
+    internal class EquipmentToSell
+    {
+        // =====================================  INSTANCE FIELDS ======================================
+
+
+        // ========================================  PROPERTIES ========================================
+
+
+        // =======================================  CONSTRUCTORS =======================================
+
+
+        // ==========================================  METHODS =========================================
+
+        /// <summary>
+        /// Blob.
+        /// </summary>
+        /// <param name="value">Blob.</param>
+        /// <returns>Blob.</returns>
+
+
+    }
+}
